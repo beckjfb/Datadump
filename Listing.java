60ac11648d3b4a7d16f3d6e89096127c862da0bc
Jason Beck <beckjfb@uga.edu>
2013-04-29 01:53:44 -0300

    more stuff

--- Listing.java
+++ Listing.java
@@ -1,20 +1,39 @@
 package helper;
 
+import java.util.ArrayList;
+
 public class Listing {
 
-	public Listing(String title, String body, int Id) {
+	public Listing(String title, String body, int id) {
 		this.Title = title;
 		this.Body = body;
-		this.Id = Id;
+		this.Id = id;
 	}	
+	
+	public Listing(String title, String body, int id, String alltags, String date){
+		this.Title = title;
+		this.Body = body;
+		this.Id = id;
+		this.allTags = alltags;
+		this.Date = date;
+		Tags = new ArrayList<String>();
+		
+		String[] splits = alltags.split(">");		
+		for(String temp:splits)
+			Tags.add(temp.substring(1));
+	}
 	
 	public Listing(String body) {
 		this.Body = body;
 	}
+	
 	private String Title;
 	private String Body;
 	private int Id;
-	
+	private String Date;
+	private String allTags;
+	private ArrayList<String> Tags;
+
 	public int getId() {
 		return Id;
 	}
@@ -33,7 +52,32 @@ public class Listing {
 	public void setBody(String body) {
 		Body = body;
 	}
-	
-	
+
+	public String getDate() {
+		if(Date != null)
+			return Date.substring(0,Date.indexOf('T')) + " " + Date.substring(11,Date.lastIndexOf('.'));
+		else
+			return null;
+	}
+
+	public void setDate(String date) {
+		Date = date;
+	}
+
+	public String getAllTags() {
+		return allTags;
+	}
+
+	public void setAllTags(String allTags) {
+		this.allTags = allTags;
+	}
+
+	public ArrayList<String> getTags() {
+		return Tags;
+	}
+
+	public void setTags(ArrayList<String> tags) {
+		Tags = tags;
+	}
 	
 }
